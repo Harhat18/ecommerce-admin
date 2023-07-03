c4504d9d432f93cb9083a8e8da44be6d9806bd11
HarunHatiboglu <harun.hatiboglu@helloworld.com.tr>
2023-07-03 18:32:09 +0300

    Add commit connect mysql

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
-import "./globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { ModalProvider } from "@/providers/modal-provider";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Admin Dashboard",
-  description: "Admin Dashboard",
+  title: "Mağaza Yönetim Paneli",
+  description: "Mağaza Yönetim Paneli",
 };
 
 export default function RootLayout({
@@ -17,7 +20,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    //TODO: create Store
+  };
+
+  return (
+    <Modal
+      title="oluşan store"
+      description="oluşan store açıklama"
+      isOpen={storeModal.isOpen}
+      onClose={() => {
+        storeModal.onClose;
+      }}
+    >
+      <div>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Adı</FormLabel>
+                    <FormControl>
+                      <Input placeholder=" E-Ticaret " {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant="outline" onClick={storeModal.onClose}>
+                  Geri Git
+                </Button>
+                <Button type="submit">Devam Et</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  );
+};
